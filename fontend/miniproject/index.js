9d813f68a451db669025502890890a55b5e61ada
Satthawut12 <satthawut.p12@gmail.com>
2021-04-25 23:41:38 +0530

    Enter

--- fontend/miniproject/index.js
+++ fontend/miniproject/index.js
@@ -1,20 +1,3 @@
-console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-   let num = Math.floor(Math.random()*21)+0;
-   console.log('Number' + ':' +num);
-   sock.on('data', function(data) {
-       console.log('DATA ' + sock.remoteAddress + ': ' + data);
-       console.log(round);
-       
-   sock.on('close', function(data) {
-       console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
-});
-client.connect(PORT, HOST, function() {
-   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-   var name = 6035512003;
-   var nameST = name.toString();
-   client.write(nameST);
-});
 
 client.on('data', function(data) {
     console.log('DATA: ' + data);
